490c2e36535eacfe6100e00c0ba6025f9537d5b0
zeldakatze <git@zeldakatze.de>
2026-02-01 11:56:36 +0100

    add some more game over stuff

--- Scripts/blob.gd
+++ Scripts/blob.gd
@@ -1,21 +1,23 @@
 extends Node2D
 
 var attack_timer: Timer;
+var reveal_timer: Timer;
 var sprite: AnimatedSprite2D;
 
 var playerHasScrewedUp = false
+var defeated = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	attack_timer = find_child("AttackTimer")
+	reveal_timer = find_child("RevealTimer")
 	sprite = find_child("AnimatedSprite2D")
 	sprite.hide()
 	
 func ActivateMonster() -> void:
-	sprite.animation = "default"
+	sprite.play("default")
 	sprite.show()
 	attack_timer.start()
-	print("ok")
 
 func MonsterRevealed() -> void:
 	pass
@@ -25,7 +27,12 @@ func TriggerJumpscare() -> void:
 	
 	sprite.apply_scale(Vector2(5, 5))
 	playerHasScrewedUp = true
-	
+
+func make_defeat() -> void:
+	defeated = true
+	attack_timer.stop()
+	reveal_timer.stop()
+	sprite.play("dying")
 
 func AnimationFinished() -> void:
 	# TODO go to game over
@@ -33,6 +40,10 @@ func AnimationFinished() -> void:
 		var container = get_node("/root/MainMenu")
 		container.show_game_over()
 	
+	if defeated:
+		var firstperson = get_parent().get_parent()
+		firstperson.hide()
+		firstperson.queue_free()
 	
 	pass
 
